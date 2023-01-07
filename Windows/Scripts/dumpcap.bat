9a4e4a1aa36cd2085c683ec652bf22a68c92394b
fsfang <16744374+fsfang@users.noreply.github.com>
2023-01-07 13:40:57 +0800

    Delete Windows directory

--- Windows/Scripts/dumpcap.bat
+++
@@ -1,3 +0,0 @@
-REM "C:\Program Files\Wireshark\dumpcap.exe" -i 9 -f 'host x.x.x.x and host x.x.x.x' -w traffic.pcap
-
-"C:\Program Files\Wireshark\dumpcap.exe" -i 9 -b filesize:50000 -w traffic.pcap
